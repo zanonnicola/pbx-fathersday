adeb0125a54a8911f3646d7f91b5bb55af498446
Leon Brocard <acme@astray.com>
2015-05-14 15:17:05 +0100

    Pass in AWS region

--- bin/sync.sh
+++ bin/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws s3 sync . s3://fathersday.photobox.co.uk --exclude bin/\* --exclude .git/\* --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --cache-control max-age=3600
+aws s3 sync . s3://fathersday.photobox.co.uk --region eu-west-1 --exclude bin/\* --exclude .git/\* --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --cache-control max-age=3600
